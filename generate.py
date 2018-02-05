11be781716832b0b968f392c766e487af110d488
Vitaly Shmaev <vitaliy.shmaev@glidewelldental.com>
2018-02-05 14:46:52 +0300

    generate.py refactoring and add docstrings

--- generate.py
+++ generate.py
@@ -9,8 +9,14 @@ import common
 import string
 
 
-# Add additional arguments
 def populate_argparser(argparser):
+    """
+    Add additional arguments
+
+    :param argparser:
+    """
+
+    # Add "count" argument
     argparser.add_argument(
         '--count',
         metavar="UINT",
@@ -18,6 +24,8 @@ def populate_argparser(argparser):
         type=int,
         help="Number of rows in table",
     )
+
+    # Add "output" argument
     argparser.add_argument(
         '--output',
         metavar="PATH",
@@ -26,16 +34,41 @@ def populate_argparser(argparser):
     )
 
 
-# Generate date list
+def create_db(db):
+    """
+    Create db and table
+
+    :param db:  DbManager object
+    """
+    if not db.execute('CREATE TABLE IF NOT EXISTS {0} ({1} {2}, {3} {4}, {5} {6}, {7} {8}, {9} {10}, {11} {12})'.
+                      format(common.CHECK_OBJECT_TABLE, common.COT_LOAD_DATE_COL, common.COT_LOAD_DATE_TYPE,
+                             common.COT_ID_COL, common.COT_ID_TYPE, common.COT_INT_COL, common.COT_INT_TYPE,
+                             common.COT_FLOAT_COL, common.COT_FLOAT_TYPE, common.COT_CHAR_COL, common.COT_CHAR_TYPE,
+                             common.COT_DATE_COL, common.COT_DATE_TYPE)):
+        raise common.TableNotCreatedException()
+    db.commit()
+
+
 def generate_datelist(now):
+    """
+    Generate date list
+
+    :param now: Today's date
+    :return: List with dates
+    """
     dates = []
     for i in range(0, common.DEFAULT_DAYS):
         dates.append(now - datetime.timedelta(days=i))
     return dates
 
 
-# Generate load_date
 def generate_load_date(dates):
+    """
+    Generate load_date
+
+    :param dates: List with dates
+    :return: LOAD_DATE value
+    """
     j = random.randint(0, len(dates))
     if j == len(dates):
         load_date = None
@@ -44,8 +77,12 @@ def generate_load_date(dates):
     return load_date
 
 
-# Generate data for ID
 def generate_id():
+    """
+    Generate data for ID
+
+    :return: ID value
+    """
     j = random.randint(-1, 10000)
     if j < 100:
         _id = None
@@ -54,8 +91,13 @@ def generate_id():
     return _id
 
 
-# Generate data for INT_VALUE
 def generate_int_value(_id):
+    """
+    Generate data for INT_VALUE
+
+    :param _id: ID value
+    :return: INT_VALUE value
+    """
     j = random.randint(-1, 10000)
     if j == 0:
         int_value = j
@@ -68,8 +110,12 @@ def generate_int_value(_id):
     return int_value
 
 
-# Generate data for FLOAT VALUE
 def generate_float_value():
+    """
+    Generate data for FLOAT VALUE
+
+    :return: FLOAT_VALUE value
+    """
     j = random.random()
     if j < 0.05:
         float_value = None
@@ -78,8 +124,12 @@ def generate_float_value():
     return float_value
 
 
-# Generate data for CHAR_VALUE
 def generate_char_value():
+    """
+    Generate data for CHAR_VALUE
+
+    :return: CHAR_VALUE value
+    """
     j = random.randint(-1, 10)
     if j == -1:
         char_value = None
@@ -88,8 +138,13 @@ def generate_char_value():
     return char_value
 
 
-# Generate data for DATE_VALUE
 def generate_date_value(dates):
+    """
+    Generate data for DATE_VALUE
+
+    :param dates: List with dates
+    :return: DATE_VALUE value
+    """
     j = random.randint(0, len(dates))
     if j == len(dates):
         date_value = None
@@ -99,8 +154,20 @@ def generate_date_value(dates):
 
 
 def insert_data_in_db(i, db, load_date, _id, int_value, float_value, char_value, date_value):
+    """
+    Insert generated data in database
 
-    #Starting new SQL transaction after some rows
+    :param i: iterator value
+    :param db: DbManager object
+    :param load_date: LOAD_DATE value
+    :param _id: ID value
+    :param int_value: INT_VALUE value
+    :param float_value: FLOAT_VALUE value
+    :param char_value: CHAR_VALUE value
+    :param date_value: DATE_VALUE value
+    """
+
+    # Starting new SQL transaction after some rows
     if (i % common.TRANSACTION_SIZE) == 0:
         db.commit()
         db.execute('BEGIN TRANSACTION')
@@ -113,8 +180,14 @@ def insert_data_in_db(i, db, load_date, _id, int_value, float_value, char_value,
                (load_date, _id, int_value, float_value, char_value, date_value))
 
 
-# Generate data for database
-def generate_data_for_db(args, dates, db):
+def generate_data_and_insert_in_db(args, dates, db):
+    """
+    Generate data for database
+
+    :param args: Arguments from command string
+    :param dates: List with dates
+    :param db: DbManager object
+    """
     for i in range(0, args.count):
         _id = generate_id()
         insert_data_in_db(i, db, generate_load_date(dates), _id, generate_int_value(_id),
@@ -123,19 +196,14 @@ def generate_data_for_db(args, dates, db):
 
 
 def main(args):
-    with DbManager(sqlite3, args.output) as db:
+    """
+    Main function
 
-        # Create db and table
-        if not db.execute('CREATE TABLE IF NOT EXISTS {0} ({1} {2}, {3} {4}, {5} {6}, {7} {8}, {9} {10}, {11} {12})'.
-                          format(common.CHECK_OBJECT_TABLE, common.COT_LOAD_DATE_COL,
-                                 common.COT_LOAD_DATE_TYPE, common.COT_ID_COL, common.COT_ID_TYPE,
-                                 common.COT_INT_COL, common.COT_INT_TYPE, common.COT_FLOAT_COL,
-                                 common.COT_FLOAT_TYPE, common.COT_CHAR_COL, common.COT_CHAR_TYPE,
-                                 common.COT_DATE_COL, common.COT_DATE_TYPE)):
-            raise common.TableNotCreatedException()
-        db.commit()
-        datelist = generate_datelist(datetime.datetime.now().date())
-        generate_data_for_db(args, datelist, db)
+    :param args: Arguments from command string
+    """
+    with DbManager(sqlite3, args.output) as db:
+        create_db(db)
+        generate_data_and_insert_in_db(args, generate_datelist(datetime.datetime.now().date()), db)
 
 
 # Insert point
